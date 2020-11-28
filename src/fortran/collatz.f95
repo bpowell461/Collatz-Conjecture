8bfe6561585ccc65d3065b70808d2608d6efd1e7
Brad Powell <bpowell461@gmail.com>
2020-11-28 15:08:56 -0500

    Added comments

--- src/fortran/collatz.f95
+++ src/fortran/collatz.f95
@@ -4,7 +4,7 @@ implicit none
 interface
 
         subroutine bubbleSort(keyArray, otherArray)
-                integer*8, dimension(10), intent(inout) :: keyArray
+                integer*8, dimension(10), intent(inout) :: keyArray 
                 integer*8, dimension(10), intent(inout) :: otherArray
 
                 integer*8 :: temp
@@ -12,7 +12,12 @@ interface
                 LOGICAL :: swapped
 
         end subroutine bubbleSort
+        
+        function loopCollatz(collatzNum) result(out)
+                integer*8 :: collatzNum
+                integer :: out
                 
+        end function loopCollatz
 end interface
 
 
@@ -26,65 +31,55 @@ end interface
 
 
        do i=1,10
-        stepArray(i) = 0
+        stepArray(i) = 0      !initializing arrays to hold 0 for comparisons
         magnitudeArray(i) = 0
        end do
  
        do while (num .ne. 0)
         
         collatzNum = num
-        steps = 0
-        isSame = 0
-        minLength = stepArray(1)
-        minNum = 1
-        newIndex = 1
-        do while (collatzNum .ne. 1)
-
-                if((mod(collatzNum,2)) == 1) then
-                        collatzNum = (collatzNum * 3) + 1
-                        
-                else
-                        collatzNum = collatzNum / 2
-                end if
-
-                steps = steps + 1
-        end do
+        steps = loopCollatz(collatzNum) !num and amount of steps in sequence
+        isSame = 0                      !duplicate checker
+        minLength = stepArray(1)        !smallest steps in seq.
+        minNum = 1                      !smallest number in seq.
+        newIndex = 1                    !used for replacing minimum steps and num
 
         do i=1, 10
-                if( minLength > stepArray(i) ) then
-                        minLength = stepArray(i)
+                if( minLength > stepArray(i) ) then         !Is the current num steps greater than the steps in the seq.?
                         minNum  = i
+                        minLength = stepArray(i)
                 end if
 
-                if ( stepArray(i) == steps) then
+                if ( stepArray(i) == steps) then        !Do the numbers have the same amount of steps? Take the smaller number. 
                         isSame = 1
                         newIndex = i               
                 end if         
         end do
         
-        j = minNum
-
-        if( steps > stepArray(j) .and. isSame == 0) then
-                magnitudeArray(j) = num
+        j = minNum                                      !Index to replace with the smaller number
+        
+        if(isSame == 0) then                            !Is it not the same?
+            if( steps > stepArray(j)) then
+                magnitudeArray(j) = num                 !Place in array accordingly
                 stepArray(j) = steps
+            end if
+        else 
+            if( num  < stepArray(newIndex)) then
+                magnitudeArray(newIndex) = num          !If it is the same then place the newest number, which will be smaller
+            end if
         end if
-
-        if( num  < stepArray(newIndex) .and. isSame == 1) then
-                magnitudeArray(newIndex) = num
-        end if
-
         num = num - 1
         end do
 
 
-        call bubbleSort(stepArray, magnitudeArray)
+        call bubbleSort(stepArray, magnitudeArray)      !Sort array and print
         print *, "Sequence Length Array (Magnitude -> Steps)"
          
         do i=10,1,-1
                 print *,  magnitudeArray(i), stepArray(i)
         end do
 
-        call bubbleSort(magnitudeArray, stepArray)
+        call bubbleSort(magnitudeArray, stepArray)      !Sort array and print
         print *, "Magnitude Array (Magnitude -> Steps)"
 
         do i=10,1, -1
@@ -94,7 +89,7 @@ end interface
 end program collatz
 
 
-subroutine bubbleSort(keyArray, otherArray)
+subroutine bubbleSort(keyArray, otherArray)         !Rosetta Code implementation of Bubble Sort
 implicit none
         integer*8, dimension(10), intent(inout) :: keyArray
         integer*8, dimension(10), intent(inout) :: otherArray
@@ -122,3 +117,20 @@ implicit none
         IF (.NOT. swapped) EXIT
         END DO
 end subroutine bubbleSort
+
+function loopCollatz(collatzNum) result(out)
+                integer*8 :: collatzNum
+                integer :: out
+                 out = 0
+                do while (collatzNum .ne. 1)            !Generating Collatz Seq. for a specific number
+
+                if((mod(collatzNum,2)) == 1) then
+                        collatzNum = (collatzNum * 3) + 1
+                        
+                else
+                        collatzNum = collatzNum / 2
+                end if
+
+                out = out + 1
+        end do
+end function loopCollatz
